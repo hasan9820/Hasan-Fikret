c4f1ef8ad3523b1ed04eb937c33bfc933d6d797c
Jon DOe <you@example.com>
2019-10-02 17:57:35 +0300

    first

---
+++ Lab1_HW1/Lab1_HW1/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_HW1
+{
+    class Calculator
+    {
+        public double Sub(double a1, double a2)
+        {
+            return a1 + a2;
+        }
+
+        public double Izv(double a1, double a2)
+        {
+            return a1 - a2;
+        }
+
+        public double Umn(double a1, double a2)
+        {
+            return a1 * a2;
+        }
+        public double Del(double a1, double a2)
+        {
+            return a1 / a2;
+        }
+    }
+}
---
+++ Lab1_HW2/Lab1_HW2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_HW2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Тази програма показва дали числото е четно или нечетно !", "Задача |2|");
+            label1.Text = "Въведе някакво число  : ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int x = Int32.Parse(textBox1.Text);
+                if(x == 0)
+                {
+                    MessageBox.Show("Мисля, че е четно", "Нали");
+                }
+                else if (x % 2 == 0)
+                {
+                    label1.Text = " Четно!";
+                }
+                else
+                {
+                    label1.Text = "Нечетно!";
+                }
+            }
+            catch
+            {
+                label1.Text = "Моля въведете валиден номер!";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Да се затвори ли приложението ? ", "Важно?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
